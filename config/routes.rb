9744b99db625a625257a588c39aa017a01626939
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-03-26 15:29:14 -0700

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :home
   devise_for :users
   resources :trips do
     resources :comments
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'trips#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
