587bc4b0b16042b325d493f09eb2197d41235143
danielmontgomery <daniel.w.montgomery@gmail.com>
2015-04-02 13:14:02 -0700

    about to delete precompiled assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   has_many :referencers, through: :references
 
   has_many :trips_organized, class_name: "Trip", foreign_key: :organizer_id, inverse_of: :organizer
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "160x160>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "160x160>", :tiny => "75x75>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
